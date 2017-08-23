44a4dbc2239a66d0bf3eb838783404f692caf6eb
Yaël Mendes <ymendes>
2017-08-23 13:47:58 +0200

    co

--- app/components/stories-map.component.ts
+++ app/components/stories-map.component.ts
@@ -3,6 +3,8 @@ import {Component, OnInit, Input} from '@angular/core';
 import {Story} from "../objects/story";
 import {Http} from "@angular/http";
 
+import {Map} from "openlayers";
+
 @Component({
   moduleId: module.id,
   selector: 'story-map',
@@ -11,11 +13,29 @@ import {Http} from "@angular/http";
 export class StoryMapComponent implements OnInit {
   @Input() stories: Story[];
 
+  map: Map;
+  simple: 'example';
+
   constructor(
     public http: Http) {
   }
 
   ngOnInit(): void {
+  }
+
+  initMap() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+      })
+    });
   }
 
 }
