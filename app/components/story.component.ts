2f41d6ad3367717295b1ce3bb5a77323cc87c7f2
Yaël Mendes <ymendes>
2017-08-29 14:18:57 +0200

    works with ngx-ol

--- app/components/story.component.ts
+++ app/components/story.component.ts
@@ -19,15 +19,43 @@ export class StoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.retrieveOneObservableStories();
-    this.retrieveObservableStories();
+  //  this.retrieveObservableStories();
+
+    this.stories = this.retrieveMockStories();
   }
+
+  retrieveMockStories() : Story[] {
+
+    let st1, st2, st3 : Story;
+
+    st1 = new Story("descr1", "addr11", new Date());
+    st2 = new Story("descr2", "addr2", new Date());
+    st3 = new Story("descr3", "addr3", new Date());
+
+    let storiesList : Story[];
+
+    storiesList = new Array();
+
+    storiesList.push(st1, st2, st3);
+
+    return storiesList;
+
+    /*
+    let storiesList___ = [
+      {id: "1", description: "birth", address: 'place Vendome 75000 Paris France', begin: '1977-10-02'},
+      {id: "2", description: "this is a short descr", address: 'place Vendome 75000 Paris France', begin: '1977-10-02'},
+      {id: "3", description: "this is another story",  address: 'place Vendome 75000 Paris France', begin: '1977-10-02'}
+    ];
+    return {st1,st2,st3};
+    */
+
+
+  }
+
 
   retrieveObservableStories(){
     this.dataService.getObservableStories()
       .subscribe(stories => this.stories = stories, err => { console.log(err);});
-
-
-
     console.log('calling retrieveStories....:'+this.stories);
   }
 
