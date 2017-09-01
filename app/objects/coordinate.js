16caf076acf27261cd17d12c99e044bf64821a2e
Yaël Mendes <ymendes>
2017-09-01 15:01:32 +0200

    c

---
+++ app/objects/coordinate.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Coordinate = (function () {
+    function Coordinate(x, y, srid) {
+        this.x = x;
+        this.y = y;
+        this.srid = srid;
+    }
+    return Coordinate;
+}());
+exports.Coordinate = Coordinate;
+//# sourceMappingURL=coordinate.js.map
